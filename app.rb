8c968a25f9c756f9b06826941aac90d98c7d440d
Peter Hattyar <peter.hattyar@gmail.com>
2022-03-28 16:31:47 +0100

    First commit

---
+++ app.rb
@@ -0,0 +1,26 @@
+# in app.rb
+
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class BookmarkManager < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Main Page'
+  end
+
+  get '/bookmarks' do
+    bookmarks = [
+              "http://www.makersacademy.com",
+              "http://www.destroyallsoftware.com",
+              "http://www.google.com"
+             ]
+  
+    bookmarks.join
+  end
+
+  run! if app_file == $0
+end
---
+++ spec/features/viewing_bookmarks_spec.rb
@@ -0,0 +1,16 @@
+feature 'Test Page' do
+  scenario 'visiting the index page' do
+    visit('/')
+    expect(page).to have_content 'Main Page'
+  end
+end
+
+feature 'Viewing bookmarks' do
+  scenario 'A user can see bookmarks' do
+    visit('/bookmarks')
+
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
+  end
+end
